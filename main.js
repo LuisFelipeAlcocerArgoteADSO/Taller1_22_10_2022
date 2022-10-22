34cc72ee1cf2d38394d544979a240a5df11c8294
LuisFelipeAlcocerArgoteADSO <luisfelipe.1998.20@gmail.com>
2022-10-22 16:46:06 -0500

    Solucion de ejercicio 1

---
+++ main.js
@@ -0,0 +1,6 @@
+
+//"Desarrolle un algoritmo que realice la sumatoria de los números enteros comprendidos entre el 1 y el 1O, es decir, 1 + 2 + 3 + .... + 1O.//
+
+let resultado= 1+2+3+4+5+6+7+8+9+10;
+alert ('La sumatoria de los números enteros es:' + resultado);
+console.log ('La sumatoria de los números enteros es:' + resultado);
